538ca10d7d79bd727457dfcc76f0d1a6addcaf91
Anand Chitipothu <anandology@gmail.com>
2018-04-17 23:52:38 +0530

    Improvements to plot_decision_boundaries.
    - Split the code into smaller functions
    - Added feature names as axes labels
    - Added class names as tick labels to the colorbar

--- modelvis.py
+++ modelvis.py
@@ -3,6 +3,7 @@ Visualising Machine Learning Models
 """
 
 import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.tree import export_graphviz
 from IPython.display import SVG
@@ -14,7 +15,8 @@ __version__ = "0.1.2"
 __author__ = "Amit Kapoor <amitkaps@gmail.com>"
 
 def plot_decision_boundaries(model, X, y,
-    probability=False, show_input=False, alpha=0.5):
+    probability=False, show_input=False,
+    feature_names=None, class_names=None, alpha=0.5):
     """Plots the decision boundaries for a classifier with 2 features.
 
     This is good way to visualize the decision boundaries of various classifiers
@@ -23,20 +25,70 @@ def plot_decision_boundaries(model, X, y,
     It is assumed that the model is already trained.
 
     :param model: the classification model
-    :param X: the training input samples
+    :param X: the training input samples as a dataframe or a 2-d array
     :param y: the target values
     :param probability: flag to indicate whether to plot class predictions or probabilities
     :param show_input: flag to indicate whether or not to show input points
+    :param feature_names: names of the columns; used to label the axes
+    :param class_names: names of the classes; used to label the colorbar
     :param alpha: the alpha value for the plots
     """
+    if isinstance(X, pd.DataFrame):
+        # take feature names from the dataframe if possible
+        if feature_names is None:
+            feature_names = X.columns
+        X = X.as_matrix()
 
-    x_min, x_max = X.iloc[:,0].min(), X.iloc[:,0].max()
-    y_min, y_max = X.iloc[:,1].min(), X.iloc[:,1].max()
+    xx, yy = _make_mesh(X, y, n=100)
+    Z = _predict_mesh(model, xx, yy, probability=probability)
+
+    if probability is False:
+        num_classes = len(np.unique(y))
+        cmap = plt.cm.get_cmap('viridis', num_classes)
+
+        ticks = range(num_classes)
+        if class_names is not None:
+            tick_labels = class_names
+        else:
+            tick_labels = ticks
+        label = "class"
+    else:
+        ticks = None
+        tick_labels = None
+        cmap = "viridis"
+        if class_names is not None:
+            label = "probability({})".format(class_names[0])
+        else:
+            label = "probability(class 0)"
+    cs = plt.contourf(xx, yy, Z, cmap=cmap, alpha=alpha)
+    colorbar = plt.colorbar(cs, ticks=ticks, label=label)
+    if tick_labels is not None:
+        colorbar.set_ticklabels(tick_labels)
+
+    if show_input:
+        plt.scatter(x = X[:,0], y = X[:,1], c=y,
+            s=20, cmap="magma", alpha=alpha)
+
+    if feature_names is not None:
+        plt.xlabel(feature_names[0])
+        plt.ylabel(feature_names[1])
+
+def _make_mesh(X, y, n=100):
+    """Makes a mesh to plot contours.
+    """
+    x_min, x_max = X[:,0].min(), X[:,0].max()
+    y_min, y_max = X[:,1].min(), X[:,1].max()
+
+    dx = (x_max - x_min)/n
+    dy = (y_max - y_min)/n
 
     xx, yy = np.meshgrid(
-        np.arange(x_min, x_max, (x_max - x_min)/100),
-        np.arange(y_min, y_max, (y_max - y_min)/100))
+        np.arange(x_min, x_max, dx),
+        np.arange(y_min, y_max, dy))
 
+    return xx, yy
+
+def _predict_mesh(model, xx, yy, probability=False):
     mesh = np.c_[xx.ravel(), yy.ravel()]
 
     if probability:
@@ -44,13 +96,7 @@ def plot_decision_boundaries(model, X, y,
     else:
         Z = model.predict(mesh)
 
-    Z = Z.reshape(xx.shape)
-    cs = plt.contourf(xx, yy, Z, cmap="viridis", alpha=alpha)
-
-    plt.colorbar(cs)
-    if show_input:
-        plt.scatter(x = X.iloc[:,0], y = X.iloc[:,1], c=y,
-            s=20, cmap="magma", alpha=alpha)
+    return Z.reshape(xx.shape)
 
 def render_tree(decision_tree, feature_names=None, class_names=None, **kwargs):
     """Returns a PIL image to visualize given DecisionTree model.
