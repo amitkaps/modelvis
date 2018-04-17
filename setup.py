0d3dfa3d029aba71ba13b82a0ee72cd5074fea42
Anand Chitipothu <anandology@gmail.com>
2018-04-17 13:34:22 +0530

    Bumped the version to 0.1.4

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
   name="modelvis",
-  version="0.1.3",
+  version="0.1.4",
   description="Visualising Machine Learning Model",
   license="MIT",
   install_requires=[
