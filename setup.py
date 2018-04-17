02d60e691584d301565cc2a22446741107e07ebd
Anand Chitipothu <anandology@gmail.com>
2018-04-17 23:51:41 +0530

    Reading the version from modelvis.py instead of duplicating it in setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,24 @@
 from setuptools import setup
+import re
+import os
+
+def get_version():
+    """Reads the version from modelvis.py.
+    """
+    root = os.path.dirname(__file__)
+    version_path = os.path.join(root, "modelvis.py")
+    text = open(version_path).read()
+    rx = re.compile("^__version__ = '(.*)'", re.M)
+    m = rx.search(text)
+    version = m.group(1)
+    return version
+
+version = get_version()
 
 setup(
   name="modelvis",
-  version="0.1.4",
-  description="Visualising Machine Learning Model",
+  version=version,
+  description="Visualising Machine Learning Models",
   license="MIT",
   install_requires=[
     'numpy',
